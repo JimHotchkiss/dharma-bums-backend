4e8909cfa9e637f0f590b009dc66f343df085a2e
github username <github email address>
2020-01-12 14:44:08 -0800

    Building out meditation controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,10 @@
 module Api::V1
   class CommentsController < ApplicationController
+
+    def index 
+      comments = Comment.find_by(meditation_id: params[:meditation_id])
+      render json: { status: 'SUCCESS', message: 'loaded comments', data: comments } 
+    end 
     def show 
       comment = Comment.find(params[:id]) 
       render json: comment, include: [:replies] 
--- app/controllers/api/v1/meditations_controller.rb
+++ app/controllers/api/v1/meditations_controller.rb
@@ -5,9 +5,36 @@ module Api::V1
       render json: { status: 'SUCCESS', message: 'loaded meditations', data: meditations } 
     end 
 
+    def create 
+      meditation = Meditation.new(meditation_params)
+      if meditation.save
+        render json: meditation
+      else 
+        render json: {error: 'Error creating meditation group'} 
+      end 
+    end 
+
     def show 
       meditation = Meditation.find(params[:id]) 
       render json: meditation, include: [:comments] 
+    end 
+
+    def destroy 
+      meditation = Meditation.find(params[:id])
+      meditation.destroy
+    end 
+
+    def update
+      meditation = Meditation.find(params[:id])
+      meditation.update(theme: params[:meditation][:theme], duration: params[:meditation][:duration], organizer: [:meditation][:organizer])
+      meditation.save
+      render json: meditation
+    end 
+
+    private 
+
+    def meditation_params
+      params.require(:meditation).permit(:theme, :duration, :organizer)
     end 
   end
 end 
