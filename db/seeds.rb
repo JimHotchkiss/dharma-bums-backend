0f3b4e6f26fa131f11a870e508ab2c465213694c
github username <github email address>
2020-02-16 06:24:09 -0800

    Added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Meditation.create(theme: "Having no head", duration: 5, organizer: "Douglas harding")
+Meditation.create(theme: "Don't be angry at anger", duration: 10, organizer: "Joseph Goldstein")
+Meditation.create(theme: "Letting go", duration: 15, organizer: "Dan Harris")
+Meditation.create(theme: "The Self is in your head", duration: 20, organizer: "Sam Harris")
+
